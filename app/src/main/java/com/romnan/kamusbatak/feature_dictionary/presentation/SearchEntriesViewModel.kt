bef44e7c6d3c2a893788468504d12fefb1678f74
Deddy Romnan <romnandeddy@gmail.com>
2022-02-11 22:29:53 +0700

    feat: create search entries viewmodel

---
+++ app/src/main/java/com/romnan/kamusbatak/feature_dictionary/presentation/SearchEntriesViewModel.kt
@@ -0,0 +1,69 @@
+package com.romnan.kamusbatak.feature_dictionary.presentation
+
+import androidx.compose.runtime.State
+import androidx.compose.runtime.mutableStateOf
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.romnan.kamusbatak.core.util.Resource
+import com.romnan.kamusbatak.feature_dictionary.domain.use_case.SearchWithBatakKeyword
+import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.Job
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.flow.MutableSharedFlow
+import kotlinx.coroutines.flow.asSharedFlow
+import kotlinx.coroutines.flow.launchIn
+import kotlinx.coroutines.flow.onEach
+import kotlinx.coroutines.launch
+import javax.inject.Inject
+
+@HiltViewModel
+class SearchEntriesViewModel @Inject constructor(
+    private val searchWithBatakKeyword: SearchWithBatakKeyword
+) : ViewModel() {
+
+    private val _searchQuery = mutableStateOf("")
+    val searchQuery: State<String> = _searchQuery
+
+    private val _state = mutableStateOf(SearchEntriesState())
+    val state: State<SearchEntriesState> = _state
+
+    private val _eventFlow = MutableSharedFlow<UIEvent>()
+    val eventFlow = _eventFlow.asSharedFlow()
+
+    private var searchJob: Job? = null
+
+    fun onSearchWithBatakKeyword(query: String) {
+        _searchQuery.value = query
+        searchJob?.cancel()
+        searchJob = viewModelScope.launch {
+            delay(500L)
+            searchWithBatakKeyword(query).onEach { result ->
+                when (result) {
+                    is Resource.Success -> {
+                        _state.value = state.value.copy(
+                            entries = result.data ?: emptyList(),
+                            isLoading = false
+                        )
+                    }
+                    is Resource.Loading -> {
+                        _state.value = state.value.copy(
+                            entries = result.data ?: emptyList(),
+                            isLoading = true
+                        )
+                    }
+                    is Resource.Error -> {
+                        _state.value = state.value.copy(
+                            entries = result.data ?: emptyList(),
+                            isLoading = false
+                        )
+                        _eventFlow.emit(UIEvent.ShowSnackbar(result.message ?: "Unknown error"))
+                    }
+                }
+            }.launchIn(this)
+        }
+    }
+
+    sealed class UIEvent {
+        data class ShowSnackbar(val message: String) : UIEvent()
+    }
+}
