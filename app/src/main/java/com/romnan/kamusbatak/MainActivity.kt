1082c62b2bd5becd742da8e814e6c12326ab2852
Deddy Romnan <romnandeddy@gmail.com>
2022-02-11 22:30:19 +0700

    feat: create user interface

--- app/src/main/java/com/romnan/kamusbatak/MainActivity.kt
+++ app/src/main/java/com/romnan/kamusbatak/MainActivity.kt
@@ -3,26 +3,76 @@ package com.romnan.kamusbatak
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
-import androidx.compose.material.Text
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.material.*
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.hilt.navigation.compose.hiltViewModel
+import com.romnan.kamusbatak.feature_dictionary.presentation.EntryItem
+import com.romnan.kamusbatak.feature_dictionary.presentation.SearchEntriesViewModel
 import com.romnan.kamusbatak.ui.theme.KamusBatakTheme
+import dagger.hilt.android.AndroidEntryPoint
+import kotlinx.coroutines.flow.collectLatest
 
+@AndroidEntryPoint
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
             KamusBatakTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colors.background
-                ) {
-                    Greeting("Android")
+                val viewModel: SearchEntriesViewModel = hiltViewModel()
+                val state = viewModel.state.value
+                val scaffoldState = rememberScaffoldState()
+
+                LaunchedEffect(key1 = true) {
+                    viewModel.eventFlow.collectLatest { event ->
+                        when (event) {
+                            is SearchEntriesViewModel.UIEvent.ShowSnackbar -> {
+                                scaffoldState.snackbarHostState.showSnackbar(
+                                    message = event.message
+                                )
+                            }
+                        }
+                    }
+                }
+
+                Scaffold(scaffoldState = scaffoldState) {
+                    Box(modifier = Modifier.background(MaterialTheme.colors.background)) {
+                        Column(
+                            modifier = Modifier
+                                .fillMaxSize()
+                                .padding(16.dp)
+                        ) {
+                            TextField(
+                                value = viewModel.searchQuery.value,
+                                onValueChange = viewModel::onSearchWithBatakKeyword,
+                                modifier = Modifier.fillMaxWidth(),
+                                placeholder = {
+                                    Text(text = "Type word here")
+                                }
+                            )
+                            Spacer(modifier = Modifier.height(16.dp))
+                            LazyColumn(
+                                modifier = Modifier.fillMaxSize()
+                            ) {
+                                items(state.entries.size) { i ->
+                                    val entry = state.entries[i]
+                                    if (i > 0) {
+                                        Spacer(modifier = Modifier.height(8.dp))
+                                    }
+                                    EntryItem(entry = entry)
+                                    if (i < state.entries.size - 1) {
+                                        Divider()
+                                    }
+                                }
+                            }
+                        }
+                    }
                 }
             }
         }
