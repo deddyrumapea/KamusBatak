5cb763486e65e166825085c92564fc786358ffb4
Deddy Romnan <romnandeddy@gmail.com>
2022-02-13 10:28:16 +0700

    feat: add languange swap

--- app/src/main/java/com/romnan/kamusbatak/MainActivity.kt
+++ app/src/main/java/com/romnan/kamusbatak/MainActivity.kt
@@ -3,17 +3,18 @@ package com.romnan.kamusbatak
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.material.*
-import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
 import com.romnan.kamusbatak.feature_dictionary.presentation.EntryItem
+import com.romnan.kamusbatak.feature_dictionary.presentation.SearchEntriesEvent
 import com.romnan.kamusbatak.feature_dictionary.presentation.SearchEntriesViewModel
 import com.romnan.kamusbatak.ui.theme.KamusBatakTheme
 import dagger.hilt.android.AndroidEntryPoint
@@ -44,54 +45,62 @@ class MainActivity : ComponentActivity() {
                 }
 
                 Scaffold(scaffoldState = scaffoldState) {
-                    Box(modifier = Modifier.background(MaterialTheme.colors.background)) {
-                        Column(
-                            modifier = Modifier
-                                .fillMaxSize()
-                                .padding(16.dp)
+                    Column(modifier = Modifier.fillMaxSize()) {
+                        TopAppBar(
+                            title = {
+                                Row(
+                                    modifier = Modifier.fillMaxWidth(),
+                                    horizontalArrangement = Arrangement.SpaceEvenly,
+                                    verticalAlignment = Alignment.CenterVertically
+                                ) {
+                                    Text(
+                                        text = state.sourceLanguage.fullName,
+                                        style = MaterialTheme.typography.subtitle1
+                                            .copy(fontWeight = FontWeight.Bold)
+                                    )
+                                    IconButton(onClick = {
+                                        viewModel.onEvent(SearchEntriesEvent.LanguagesSwap)
+                                    }) {
+                                        Icon(
+                                            painterResource(id = R.drawable.ic_outline_sync_alt_24),
+                                            getString(R.string.swap_languages)
+                                        )
+                                    }
+                                    Text(
+                                        text = state.targetLanguage.fullName,
+                                        style = MaterialTheme.typography.subtitle1
+                                            .copy(fontWeight = FontWeight.Bold)
+                                    )
+                                }
+                            },
+                            backgroundColor = MaterialTheme.colors.primary
+                        )
+
+                        TextField(
+                            value = viewModel.searchQuery.value,
+                            modifier = Modifier.fillMaxWidth(),
+                            placeholder = { Text(text = getString(R.string.enter_words_here)) },
+                            onValueChange = {
+                                viewModel.onEvent(SearchEntriesEvent.QueryChange(it))
+                            }
+                        )
+
+                        Spacer(modifier = Modifier.height(16.dp))
+
+                        LazyColumn(
+                            modifier = Modifier.fillMaxSize()
                         ) {
-                            TextField(
-                                value = viewModel.searchQuery.value,
-                                onValueChange = viewModel::onSearchWithBatakKeyword,
-                                modifier = Modifier.fillMaxWidth(),
-                                placeholder = {
-                                    // TODO: extract string resource
-                                    Text(text = "Type word here")
-                                }
-                            )
-                            Spacer(modifier = Modifier.height(16.dp))
-                            LazyColumn(
-                                modifier = Modifier.fillMaxSize()
-                            ) {
-                                items(state.entries.size) { i ->
-                                    val entry = state.entries[i]
-                                    if (i > 0) {
-                                        Spacer(modifier = Modifier.height(8.dp))
-                                    }
-                                    EntryItem(entry = entry)
-                                    if (i < state.entries.size - 1) {
-                                        Divider()
-                                    }
-                                }
+                            items(state.entries.size) { i ->
+                                if (i > 0) Spacer(modifier = Modifier.height(8.dp))
+                                EntryItem(entry = state.entries[i])
+                                Spacer(modifier = Modifier.height(8.dp))
+                                if (i < state.entries.size - 1) Divider()
                             }
                         }
                     }
                 }
+                if (state.isLoading) CircularProgressIndicator()
             }
         }
-    }
-}
-
-// TODO: delete unused composables
-@Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
-}
-
-@Preview(showBackground = true)
-@Composable
-fun DefaultPreview() {
-    KamusBatakTheme {
-        Greeting("Android")
     }
 }
