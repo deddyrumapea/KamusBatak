18eedf37eff2c1426a1d5fbd88681dfb26136f61
Deddy Romnan <romnandeddy@gmail.com>
2022-02-13 10:27:12 +0700

    fix: improve design

--- app/src/main/java/com/romnan/kamusbatak/feature_dictionary/presentation/EntryItem.kt
+++ app/src/main/java/com/romnan/kamusbatak/feature_dictionary/presentation/EntryItem.kt
@@ -3,10 +3,11 @@ package com.romnan.kamusbatak.feature_dictionary.presentation
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
@@ -15,19 +16,32 @@ import com.romnan.kamusbatak.feature_dictionary.domain.model.Entry
 @Composable
 fun EntryItem(entry: Entry, modifier: Modifier = Modifier) {
     // TODO: improve item design
-    Column(modifier = modifier) {
+    Column(modifier = modifier.padding(16.dp)) {
+        entry.dialect?.let { Text(text = it, style = MaterialTheme.typography.caption) }
+
         Text(
             text = entry.btkWord,
             fontSize = 24.sp,
-            fontWeight = FontWeight.Bold,
-            color = Color.Black
+            style = MaterialTheme.typography.h6,
+            color = MaterialTheme.colors.onSurface,
+            fontWeight = FontWeight.Bold
         )
-        Text(text = entry.phonetic ?: "", fontWeight = FontWeight.Light)
-        Spacer(modifier = Modifier.height(16.dp))
-        Text(text = entry.dialect ?: "")
-        Spacer(modifier = Modifier.height(16.dp))
-        Text(text = entry.indWord)
-        Spacer(modifier = Modifier.height(16.dp))
-        Text(text = entry.example ?: "")
+
+        entry.phonetic?.let {
+            Text(
+                text = "/$it/",
+                style = MaterialTheme.typography.subtitle1,
+                color = MaterialTheme.colors.onSurface,
+                fontWeight = FontWeight.Light
+            )
+        }
+
+        Spacer(modifier = Modifier.height(8.dp))
+        Text(text = entry.indWord, style = MaterialTheme.typography.body1)
+
+        entry.example?.let {
+            Spacer(modifier = Modifier.height(16.dp))
+            Text(text = it, style = MaterialTheme.typography.body1)
+        }
     }
 }
