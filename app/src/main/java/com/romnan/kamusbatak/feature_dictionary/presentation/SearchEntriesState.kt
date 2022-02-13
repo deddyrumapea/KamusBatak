b8c0782534dcc0823fdd4ff29a4196e263b8048d
Deddy Romnan <romnandeddy@gmail.com>
2022-02-13 10:27:53 +0700

    feat: add src and target lang

--- app/src/main/java/com/romnan/kamusbatak/feature_dictionary/presentation/SearchEntriesState.kt
+++ app/src/main/java/com/romnan/kamusbatak/feature_dictionary/presentation/SearchEntriesState.kt
@@ -1,8 +1,11 @@
 package com.romnan.kamusbatak.feature_dictionary.presentation
 
+import com.romnan.kamusbatak.feature_dictionary.data.util.Language
 import com.romnan.kamusbatak.feature_dictionary.domain.model.Entry
 
 data class SearchEntriesState(
     val entries: List<Entry> = emptyList(),
-    val isLoading: Boolean = false
+    val isLoading: Boolean = false,
+    val sourceLanguage: Language = Language.Btk,
+    val targetLanguage: Language = Language.Ind
 )
